6fc1a6e726fb6f5b43c552c79f90a6f48dcb04de
Evgeniy Goncharenko <iterlace@gmail.com>
2020-11-26 13:43:36 +0200

    timetable models & commands added

--- knu_helper_bot/database.py
+++ knu_helper_bot/database.py
@@ -3,7 +3,7 @@ import logging
 
 import sqlalchemy as sqa
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData, Table, Column, Sequence
+from sqlalchemy import MetaData, Table, Column, Sequence, UniqueConstraint
 from sqlalchemy.sql.sqltypes import *
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy import ForeignKey
@@ -74,8 +74,100 @@ class StudentsGroup(Base):
     )
 
     students = relationship("User", order_by=User.tg_id, back_populates="students_group")
+    timetable_lessons = relationship("TimetableLesson", back_populates="students_group")
 
     def __repr__(self):
         return "<StudentsGroup(id={}, name={})>".format(self.id, self.name)
 
+
+class TimetableLesson(Base):
+    __tablename__ = "timetable_lessons"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    day_of_week = Column(
+        SmallInteger,
+        nullable=False,
+    )
+    starts_at = Column(
+        Time,
+        nullable=False,
+    )
+    ends_at = Column(
+        Time,
+        nullable=False,
+    )
+    students_group_id = Column(
+        Integer,
+        ForeignKey("students_groups.id"),
+        nullable=False,
+    )
+    teacher_id = Column(
+        Integer,
+        ForeignKey("teachers.id"),
+        nullable=False,
+    )
+    lesson_id = Column(
+        Integer,
+        ForeignKey("lessons.id"),
+        nullable=False,
+    )
+
+    students_group = relationship("StudentsGroup", back_populates="timetable_lessons")
+    teacher = relationship("Teacher")
+    lesson = relationship("Lesson")
+
+    __table_args__ = (
+        # UniqueConstraint("day_of_week", "students_group_id", name="timetable_dow_students_group_key"),
+    )
+
+    def __repr__(self):
+        return "<TimetableDay(id={}, day_of_week={}, group={})>"\
+            .format(self.id, self.day_of_week, self.students_group_id)
+
+
+class Lesson(Base):
+    __tablename__ = "lessons"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    name = Column(
+        String,
+        nullable=False,
+    )
+
+    def __repr__(self):
+        return "<Lesson(id={}, name={})>".format(self.id, self.name)
+
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    first_name = Column(
+        String,
+        nullable=False,
+    )
+    last_name = Column(
+        String,
+        nullable=False,
+    )
+    middle_name = Column(
+        String,
+        nullable=False,
+    )
+
+    def __repr__(self):
+        return "<Lesson(id={}, first_name={}, last_name={}, middle_name={})>"\
+            .format(self.id, self.first_name, self.last_name, self.middle_name)
+
+    def full_name(self):
+        return " ".join((self.last_name, self.first_name, self.middle_name))
 
--- knu_helper_bot/main.py
+++ knu_helper_bot/main.py
@@ -5,10 +5,13 @@ from telegram.ext import (
     Updater,
     Filters,
     CommandHandler,
+    MessageHandler,
     CallbackQueryHandler,
+    ConversationHandler,
 )
 
-from knu_helper_bot import config, commands, states
+from knu_helper_bot import config, commands, dictionaries
+from dictionaries import states
 
 logger = logging.getLogger(__name__)
 
@@ -17,14 +20,65 @@ def main():
     updater = Updater(config.BOT_TOKEN, workers=config.BOT_WORKERS)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", commands.start))
+    dp.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("start", commands.start)],
+            states={
+                states.UserSelectStudentsGroupStep: [
+                    CallbackQueryHandler(commands.select_students_group)
+                ],
+            },
+            fallbacks=[CommandHandler("start", commands.start)]
+        ),
+    )
     dp.add_handler(CommandHandler("help", commands.help))
     dp.add_handler(CommandHandler("change_group", commands.select_students_group))
+    dp.add_handler(CommandHandler("show_timetable", commands.show_timetable))
 
+    # dp.add_handler(CallbackQueryHandler(
+    #     commands.select_students_group,
+    #     states.UserSelectStudentsGroupStep.parse_pattern,
+    # ))
+    dp.add_handler(CommandHandler("edit_timetable", commands.edit_timetable))
     dp.add_handler(CallbackQueryHandler(
-        commands.select_students_group,
-        states.UserSelectStudentsGroupStep.parse_pattern,
-    ))
+                        commands.edit_timetable_callback,
+                        pattern=states.EditTimetable.parse_pattern,
+                    ))
+    dp.add_handler(CallbackQueryHandler(
+                        commands.edit_timetable_day_callback,
+                        pattern=states.EditTimetableDay.parse_pattern,
+                    ))
+    # Edit a day of a timetable
+    dp.add_handler(
+        ConversationHandler(
+            entry_points=[CallbackQueryHandler(
+                commands.add_lesson_callback,
+                pattern=states.AddLessonToTimetable.parse_pattern,
+            )],
+            states={
+                "read_lesson": [
+                    CallbackQueryHandler(
+                        commands.add_lesson_lesson_callback,
+                        pass_user_data=True,
+                    ),
+                ],
+                "read_teacher": [
+                    CallbackQueryHandler(
+                        commands.add_lesson_teacher_callback,
+                        pass_user_data=True,
+                    ),
+                ],
+                "read_time": [
+                    MessageHandler(
+                        Filters.text,
+                        commands.add_lesson_time,
+                        pass_user_data=True,
+                    ),
+                ],
+            },
+            fallbacks=[],
+        ),
+    )
 
     updater.start_polling()
 
