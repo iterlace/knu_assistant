66efe49265c829982ed2d7f3afbfe7a95b320529
Evgeniy Goncharuck <iterlace@gmail.com>
2021-02-15 22:31:58 +0200

    celery timezone fixed

--- assistant/tasks/config.py
+++ assistant/tasks/config.py
@@ -4,10 +4,11 @@ from celery.schedules import crontab
 from assistant import config
 
 app = Celery("knu_assistant", broker="sqla+{}".format(config.DB_STRING))
+app.conf.timezone = "Europe/Kiev"
 app.conf.beat_schedule = {
     "tomorrow-timetable": {
         "task": "assistant.tasks.scheduled.tomorrow_timetable",
-        "schedule": crontab(hour=22, minute=30),
+        "schedule": crontab(hour=22, minute=35),
         "args": [],
     }
 }
