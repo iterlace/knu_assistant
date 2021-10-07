b1c39960dc9ecef24f9f25efbca6465753a00ac2
Evgeniy Goncharuck <iterlace@gmail.com>
2021-10-08 02:14:56 +0300

    structural refactoring; wrapped into docker; poetry is now used

--- assistant/__init__.py
+++
@@ -1,4 +0,0 @@
-""" KNU Assistant Bot """
-
-__version__ = '0.1.1'
-__version_info__ = tuple([int(num) for num in __version__.split('.')])
--- assistant/bot/commands/utils.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-from assistant.bot.dictionaries import states
-
-
-def end(update: Update, ctx: CallbackContext):
-    ctx.user_data.clear()
-    return states.END
